bdc39c37bb67608ebeaf701209a63c64c5de28ca
lilinsen <lilinsen@jd.com>
2019-10-10 15:15:27 +0800

    增加输出路径等

--- .bin/webpack.base.js
+++ .bin/webpack.base.js
@@ -3,11 +3,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const chalk= require('chalk');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+const config               = require('./../package.json');
 module.exports = {    
   entry: './src/app.ts',
   output: {
-    filename: './js/app.js',
-    path: path.resolve(__dirname, '../dist')
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../build'+ '/' + config.version),
+    publicPath: config.publicPath + '/'+config.version+'/',
   }, 
   resolve: {
     extensions: [ '.tsx', '.ts', '.js', '.vue','.svg' ]
